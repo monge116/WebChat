eba3258d7ed55df9edd751e56925a5ebc318dff9
rodri <rodrigorodriguezmonge@gmail.com>
2024-05-08 19:53:26 +0200

    javascript

--- script.js
+++ script.js
@@ -17,4 +17,26 @@ document.addEventListener("click", (event) => {
         dropdown.classList.add('dropdown__wrapper--fade-in');
     }
 });
+login = document.getElementById("login_form");
+signup = document.getElementById("sign_up_form")
+buttons = document.getElementById("log_out")
+perfil = document.getElementById("log_in")
+function fLoginWrapper(){
+    login.style.display = "grid"
+    signup.style.display = "none"
+}
+function fSignUpWrapper(){
+    signup.style.display = "grid"
+    login.style.display = "none"
+}
+function floggedIn(){
+    
+    if(document.getElementById("email").value != null && document.getElementById("password").value != null){
+        console.log("hola")
+        // login.style.display = "none"
+        buttons.style.display = "none"
+        perfil.style.display = "flex"
+    }
+    
+}
 
